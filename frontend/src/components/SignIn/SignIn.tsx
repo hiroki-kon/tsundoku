7dc6500999ba252bf493623e54fe87feb01d5e76
hiroki-kon <hiroki-kon@users.noreply.github.com>
2024-05-23 18:39:22 +0900

    signupとsignin作成

---
+++ frontend/src/components/SignIn/SignIn.tsx
@@ -0,0 +1,50 @@
+import { TextInput, Checkbox, Button, Group, Box } from "@mantine/core";
+import { useForm } from "@mantine/form";
+
+import classes from "./SignIn.module.css";
+
+interface Props {
+  onSubmit: (values: { email: string; password: string }) => void;
+}
+
+export const SignIn = ({ onSubmit }: Props) => {
+  const form = useForm({
+    mode: "uncontrolled",
+    initialValues: {
+      email: "",
+      name: "",
+      password: "",
+    },
+
+    // validate: {
+    //   email: (value) =>
+    //     /^\S+@\S+$/.test(value) ? null : "無効なメールアドレスです",
+    //   password: (value) =>
+    //     value.length < 5 ? "パスワードは6文字以上入力してください" : null,
+    //   name: (value) => (value.length < 1 ? "名前を入力してください" : null),
+    // },
+  });
+  return (
+    <Box maw={340} mx="auto">
+      <form className={classes.form} onSubmit={form.onSubmit(onSubmit)}>
+        <TextInput
+          withAsterisk
+          label="メールアドレス"
+          placeholder="メールアドレス"
+          key={form.key("email")}
+          {...form.getInputProps("email")}
+        />
+
+        <TextInput
+          withAsterisk
+          label="パスワード"
+          placeholder="パスワード"
+          key={form.key("password")}
+          {...form.getInputProps("password")}
+        />
+
+        <Button type="submit">サインアップ</Button>
+      </form>
+    </Box>
+  );
+};
---
+++ frontend/src/components/SignUp/SignUp.tsx
@@ -0,0 +1,57 @@
+import { TextInput, Checkbox, Button, Group, Box } from "@mantine/core";
+import { useForm } from "@mantine/form";
+import classes from "./SignUp.module.css";
+
+interface Props {
+  onSubmit: (values: { name: string; email: string; password: string }) => void;
+}
+
+export const SignUp = ({ onSubmit }: Props) => {
+  const form = useForm({
+    mode: "uncontrolled",
+    initialValues: {
+      email: "",
+      name: "",
+      password: "",
+    },
+
+    // validate: {
+    //   email: (value) =>
+    //     /^\S+@\S+$/.test(value) ? null : "無効なメールアドレスです",
+    //   password: (value) =>
+    //     value.length < 5 ? "パスワードは6文字以上入力してください" : null,
+    //   name: (value) => (value.length < 1 ? "名前を入力してください" : null),
+    // },
+  });
+  return (
+    <Box maw={340} mx="auto">
+      <form className={classes.form} onSubmit={form.onSubmit(onSubmit)}>
+        <TextInput
+          withAsterisk
+          label="ユーザ名"
+          placeholder="ユーザ名"
+          key={form.key("name")}
+          {...form.getInputProps("name")}
+        />
+
+        <TextInput
+          withAsterisk
+          label="メールアドレス"
+          placeholder="メールアドレス"
+          key={form.key("email")}
+          {...form.getInputProps("email")}
+        />
+
+        <TextInput
+          withAsterisk
+          label="パスワード"
+          placeholder="パスワード"
+          key={form.key("password")}
+          {...form.getInputProps("password")}
+        />
+
+        <Button type="submit">サインアップ</Button>
+      </form>
+    </Box>
+  );
+};
